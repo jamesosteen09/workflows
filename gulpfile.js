93b426f48be743f73fbb9ef38709ee0eb845867b
James Osteen <jeo_james1984@yahoo.com>
2017-06-10 12:23:41 -0500

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = ['components/scripts/rclicks,js', 'components/scripts/pixgrid.js', 'components/scripts/tagline.js', 'components/scripts/template.js'];
@@ -16,6 +17,8 @@ gulp.task('coffee', function(){
 gulp.task('js', function() {
 gulp.src(jsSources)
 	.pipe(concat('script.js'))
+	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
 
 });
+
