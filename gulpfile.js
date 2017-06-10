6833e4f75f82a7c837b33d38226e1ebfed26f496
James Osteen <jeo_james1984@yahoo.com>
2017-06-10 18:26:21 -0500

    Turned Sass to CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	concat = require('gulp-concat'),
-	browserify = require('gulp-browserify');
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = ['components/scripts/rclicks,js', 'components/scripts/pixgrid.js', 'components/scripts/tagline.js', 'components/scripts/template.js'];
+var sassSources =['component/sass/style.scss'];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
 		.pipe(coffee({bare: true})
-			.on('error', gutil.log))
+		.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'))
 });
 
@@ -21,4 +23,18 @@ gulp.src(jsSources)
 	.pipe(gulp.dest('builds/development/js'))
 
 });
+
+gulp.task('compass', function() {
+gulp.src(sassSources)
+	.pipe(compass({
+		sass: 'components/sass',
+		image: 'builds/development/images',
+		style: 'expanded'
+
+	}))
+	.on('error', gutil.log)
+	.pipe(gulp.dest('builds/development/css'))
+
+});
+
 
